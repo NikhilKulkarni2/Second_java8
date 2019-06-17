20349ef667c443d696bf9707f8a45b6c37e353e4
Nikhil Kullkarni <46192372+NikhilKulkarni2@users.noreply.github.com>
2019-06-17 16:50:15 +0530

    Added newly
    To check desktop GitHub Working

---
+++ Second_java8/src/NewClassAdded.java
@@ -0,0 +1,9 @@
+
+public class NewClassAdded {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is add to check Desktop GitHub Working");
+	}
+
+}
